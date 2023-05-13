c045a76c584e2542ef2d50d681b6486707f893ba
Christian <christian.nguyen@beyondedge.co>
2023-05-13 16:13:17 +0700

    Te

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState()
+  async function logJSONData() {
+    const response = await fetch("https://dummyjson.com/products/1");
+    const jsonData = await response.json();
+    setData(jsonData)
+    console.log(jsonData);
+  }
+  useEffect(() => {
+    logJSONData();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {JSON.stringify(data)}
         </p>
-        <a
+        <button onClick={() => logJSONData()}>Call api</button>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
