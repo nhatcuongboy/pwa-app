250b7fb7f914f94d566af19286d97f73618a8322
Christian <christian.nguyen@beyondedge.co>
2023-05-13 18:32:19 +0700

    ad

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
 function App() {
-  const [data, setData] = useState()
-  async function logJSONData() {
-    const response = await fetch("https://dummyjson.com/products/1");
-    const jsonData = await response.json();
-    setData(jsonData)
-    console.log(jsonData);
-  }
-  useEffect(() => {
-    logJSONData();
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Data</h1>
-        <p>
-          {JSON.stringify(data)}
-        </p>
-        <button onClick={() => logJSONData()}>Call api</button>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Home.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+    const [data, setData] = useState()
+    async function logJSONData() {
+        const response = await fetch("https://dummyjson.com/products/1");
+        const jsonData = await response.json();
+        setData(jsonData)
+        console.log(jsonData);
+    }
+    useEffect(() => {
+        logJSONData();
+    }, [])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Data</h1>
+                <p>
+                    {JSON.stringify(data)}
+                </p>
+                <button onClick={() => logJSONData()}>Call api</button>
+                {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+            </header>
+        </div>
+    );
+}
+
+export default Home;
