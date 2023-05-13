937e9c6b8264fda4bee6576c8371e7f160763b87
Christian <christian.nguyen@beyondedge.co>
2023-05-13 19:03:37 +0700

    ad

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function Home() {
+    function sendPushNotification() {
+        if ('Notification' in window && 'PushManager' in window) {
+            if (Notification.permission === 'granted') {
+                navigator.serviceWorker.ready.then((registration) => {
+                    registration.pushManager.getSubscription().then((subscription) => {
+                        if (subscription) {
+                            const notificationPayload = {
+                                title: 'Thông báo',
+                                body: 'Nội dung thông báo'
+                                // icon: 'path/to/icon.png',
+                            };
+                            const notification = new Notification(JSON.stringify(notificationPayload));
+                            // subscription.pushManager
+                            //     .sendNotification(JSON.stringify(notificationPayload))
+                            //     .then(() => {
+                            //         console.log('Đã gửi thông báo đẩy thành công.');
+                            //     })
+                            //     .catch((error: any) => {
+                            //         console.error('Lỗi khi gửi thông báo đẩy:', error);
+                            //     });
+                        }
+                    });
+                });
+            }
+        }
+    }
+
     const [data, setData] = useState()
     async function logJSONData() {
         const response = await fetch("https://dummyjson.com/products/1");
@@ -22,6 +49,7 @@ function Home() {
                     {JSON.stringify(data)}
                 </p>
                 <button onClick={() => logJSONData()}>Call api</button>
+                <button onClick={sendPushNotification}>Send noti</button>
                 {/* <a
           className="App-link"
           href="https://reactjs.org"
